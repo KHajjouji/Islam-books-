c5fc2ca2a8df0fb8c324664a3e06f1958ab5410d
KHajjouji <hypocritic2002@gmail.com>
2026-08-16 21:45:40 +0200

    feat: load commercial bookstore theme modules

--- wordpress/wp-content/themes/illuminated-path/functions.php
+++ wordpress/wp-content/themes/illuminated-path/functions.php
@@ -9,11 +9,13 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'IP_THEME_VERSION', '0.1.0' );
+define( 'IP_THEME_VERSION', '0.2.0' );
 define( 'IP_THEME_DIR', get_template_directory() );
 define( 'IP_THEME_URI', get_template_directory_uri() );
 
 require_once IP_THEME_DIR . '/inc/setup.php';
 require_once IP_THEME_DIR . '/inc/multilingual.php';
 require_once IP_THEME_DIR . '/inc/woocommerce.php';
+require_once IP_THEME_DIR . '/inc/catalog.php';
+require_once IP_THEME_DIR . '/inc/patterns.php';
 require_once IP_THEME_DIR . '/inc/customizer.php';
