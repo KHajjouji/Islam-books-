a71edac430d788f8f00bdafceac35f8428ca02f0
KHajjouji <hypocritic2002@gmail.com>
2026-08-17 21:12:33 +0200

    Load visual-builder theme integration

--- wordpress/wp-content/themes/illuminated-path/functions.php
+++ wordpress/wp-content/themes/illuminated-path/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'IP_THEME_VERSION', '0.3.0' );
+define( 'IP_THEME_VERSION', '0.4.0' );
 define( 'IP_THEME_DIR', get_template_directory() );
 define( 'IP_THEME_URI', get_template_directory_uri() );
 
@@ -19,3 +19,4 @@ require_once IP_THEME_DIR . '/inc/woocommerce.php';
 require_once IP_THEME_DIR . '/inc/catalog.php';
 require_once IP_THEME_DIR . '/inc/patterns.php';
 require_once IP_THEME_DIR . '/inc/customizer.php';
+require_once IP_THEME_DIR . '/inc/elementor.php';
